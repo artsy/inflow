822a105b7db48a28fbed8679e05e2307e70ed775
Barry Hoggard <barry@hoggard.org>
2019-06-19 00:18:24 -0400

    set up circleci

--- mix.exs
+++ mix.exs
@@ -43,7 +43,8 @@ defmodule Inflow.MixProject do
       {:gettext, "~> 0.11"},
       {:jason, "~> 1.0"},
       {:plug_cowboy, "~> 2.0"},
-      {:httpoison, "~> 1.4"}
+      {:httpoison, "~> 1.4"},
+      {:credo, "~> 1.0.0", only: [:dev, :test], runtime: false}
     ]
   end
 
